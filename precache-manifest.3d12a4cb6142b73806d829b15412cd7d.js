0bd29946ba04063a0379efae9eadcccba9ff0bbd
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-09-25 16:36:59 +0100

    Updates

---
+++ precache-manifest.3d12a4cb6142b73806d829b15412cd7d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e2d7dcf93edb565e2a9cc59291be76",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "45dc32b4121ce3ca3b05",
+    "url": "/to-do/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "32234584088c021c2c70",
+    "url": "/to-do/static/css/main.e4a8a4a5.chunk.css"
+  },
+  {
+    "revision": "45dc32b4121ce3ca3b05",
+    "url": "/to-do/static/js/2.7de89f03.chunk.js"
+  },
+  {
+    "revision": "a368d37026c15d2dd934735636cd6e54",
+    "url": "/to-do/static/js/2.7de89f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32234584088c021c2c70",
+    "url": "/to-do/static/js/main.2c0fc9dd.chunk.js"
+  },
+  {
+    "revision": "b5d210bec7b313800160",
+    "url": "/to-do/static/js/runtime-main.2b5910fc.js"
+  },
+  {
+    "revision": "1e114d0e2f9aa0e1ae0ef5421a1b17fe",
+    "url": "/to-do/static/media/add.1e114d0e.svg"
+  },
+  {
+    "revision": "ed4b8ae607dd5e07930dd5e882442944",
+    "url": "/to-do/static/media/adjust.ed4b8ae6.svg"
+  },
+  {
+    "revision": "7dca83e5ac2ecacb13102beea3f0f920",
+    "url": "/to-do/static/media/arrow-back.7dca83e5.svg"
+  },
+  {
+    "revision": "66a1ba3c08c477e160f339825443c0eb",
+    "url": "/to-do/static/media/arrow-next.66a1ba3c.svg"
+  },
+  {
+    "revision": "4e55eb61847229703aeac2fa3dd8d2eb",
+    "url": "/to-do/static/media/bin.4e55eb61.svg"
+  },
+  {
+    "revision": "9a951c3456ad0ca17ea56a5fd0eb0a7a",
+    "url": "/to-do/static/media/inbox.9a951c34.svg"
+  },
+  {
+    "revision": "42a51ab38c7644f9a025443dd4a46fc2",
+    "url": "/to-do/static/media/list.42a51ab3.svg"
+  },
+  {
+    "revision": "f300ce08d077e25c811fdfd1afce2bdf",
+    "url": "/to-do/static/media/more.f300ce08.svg"
+  },
+  {
+    "revision": "10cab673e95757710417f991648769a0",
+    "url": "/to-do/static/media/search.10cab673.svg"
+  },
+  {
+    "revision": "9774f77093b5173cea32598a1aa6d1dc",
+    "url": "/to-do/static/media/star.9774f770.svg"
+  }
+]);

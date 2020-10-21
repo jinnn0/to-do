5ca809eedddd7b10b46d91b570738b31632f7fa9
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-10-22 00:21:25 +0100

    Update Add sampleTodoList.js & GlobalState refactoring

--- src/contexts/GlobalState.js
+++ src/contexts/GlobalState.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, createContext } from 'react';
-import shortid from 'shortid';
+import sampleTodoList from '../utils/sampleTodoList';
 
 export const GlobalContext = createContext();
 
 export const GlobalContextProvider = (props) => {
-  // date state
+  // today
   let today = {
     year: new Date().getFullYear(),
     month: new Date().getMonth(),
@@ -12,36 +12,6 @@ export const GlobalContextProvider = (props) => {
     date: new Date().getDate(),
     day: new Date().toLocaleString('default', { weekday: 'short' })
   };
-
-  // sample todoList
-  // prettier-ignore
-  const sampleTasks = ["Daily web development", "Get groceries for dinner", "Home exercise at 6pm", "Call Daniel for meeting", "House cleaning", "Tennis practice", "Do lundary", "Email Noah for update", "Finish monthly growth report", "Send in cacenllation letter", "Figure out vacation destination", "Fill in scholarship application" ]
-  const todoTypes = ['important', 'work', 'study', 'other'];
-
-  const sampleTodoList = sampleTasks.map((task, index) => {
-    let randomDate;
-    let randomType;
-    if (index < 2) {
-      randomDate = today.date;
-      randomType = todoTypes[0];
-    } else {
-      randomDate = today.date + Math.floor(Math.random() * 7);
-      randomType = todoTypes[Math.floor(Math.random() * 4)];
-    }
-
-    return {
-      id: shortid.generate(),
-      task: task,
-      type: randomType,
-      completed: Math.random() >= 0.5,
-      dateInfo: {
-        year: today.year,
-        month: today.month,
-        date: randomDate,
-        day: new Date(today.year, today.month, randomDate).toLocaleString('default', { weekday: 'long' })
-      }
-    };
-  });
 
   // todoList state
   const initialTodoList = JSON.parse(localStorage.getItem('to-do-list')) || sampleTodoList;
@@ -56,7 +26,8 @@ export const GlobalContextProvider = (props) => {
   };
 
   const removeTodo = (id) => {
-    setTodoList(todoList.filter((todo) => id !== todo.id));
+    const updatedTodo = todoList.filter((todo) => id !== todo.id);
+    setTodoList(updatedTodo);
   };
 
   const toggleComplete = (id) => {
@@ -68,31 +39,6 @@ export const GlobalContextProvider = (props) => {
     });
 
     setTodoList(updatedTodo);
-  };
-
-  // isAddNewTodoClicked state
-  const [isAddNewTodoClicked, setIsAddNewTodoClicked] = useState(false);
-
-  const showAddNewTodoForm = () => {
-    setIsAddNewTodoClicked(!isAddNewTodoClicked);
-  };
-
-  const hideAddNewTodoForm = () => {
-    if (isAddNewTodoClicked) {
-      setIsAddNewTodoClicked(!isAddNewTodoClicked);
-    }
-  };
-
-  // view state (daily / weekly / monthly)
-  const initialView = JSON.parse(localStorage.getItem('selected-view')) || 'daily';
-  const [selectedView, setSelectedView] = useState(initialView);
-
-  useEffect(() => {
-    localStorage.setItem('selected-view', JSON.stringify(selectedView));
-  }, [selectedView]);
-
-  const updateSelectedView = (newValue) => {
-    setSelectedView(newValue);
   };
 
   // sort state (recent / tag / oldest / completed / active)
@@ -107,10 +53,16 @@ export const GlobalContextProvider = (props) => {
     setSelectedSort(newSort);
   };
 
-  // sorted list
-  let sortedTodoList = todoList;
-  if (selectedSort === 'newest') {
-    sortedTodoList = todoList.reverse();
+  // sorted todoList
+  let sortedTodoList;
+  if (selectedSort === 'oldest') {
+    sortedTodoList = todoList.sort((a, b) => {
+      return a.timestamp - b.timestamp;
+    });
+  } else if (selectedSort === 'newest') {
+    sortedTodoList = todoList.sort((a, b) => {
+      return b.timestamp - a.timestamp;
+    });
   } else if (selectedSort === 'tag') {
     const ordering = {};
     const sortOrder = ['important', 'work', 'study', 'other'];
@@ -126,6 +78,43 @@ export const GlobalContextProvider = (props) => {
     sortedTodoList = todoList.filter((todo) => !todo.completed);
   }
 
+  const generateTodos = (todos) => {
+    return todos.filter(
+      (todo) =>
+        todo.dateInfo.year === today.year &&
+        todo.dateInfo.month === today.month &&
+        todo.dateInfo.date === today.date
+    );
+  };
+
+  const unsortedTodosToday = generateTodos(todoList);
+  const sortedTodosToday = generateTodos(sortedTodoList);
+
+  // view state (daily / weekly / monthly)
+  const initialView = JSON.parse(localStorage.getItem('selected-view')) || 'daily';
+  const [selectedView, setSelectedView] = useState(initialView);
+
+  useEffect(() => {
+    localStorage.setItem('selected-view', JSON.stringify(selectedView));
+  }, [selectedView]);
+
+  const updateSelectedView = (newValue) => {
+    setSelectedView(newValue);
+  };
+
+  // isAddNewTodoClicked state
+  const [isAddNewTodoClicked, setIsAddNewTodoClicked] = useState(false);
+
+  const showAddNewTodoForm = () => {
+    setIsAddNewTodoClicked(!isAddNewTodoClicked);
+  };
+
+  const hideAddNewTodoForm = () => {
+    if (isAddNewTodoClicked) {
+      setIsAddNewTodoClicked(!isAddNewTodoClicked);
+    }
+  };
+
   // global states
   const value = {
     today, // HomeMain, TimeDisplay, Daily, Weekly, Monthly, AddNewTodoForm
@@ -133,15 +122,17 @@ export const GlobalContextProvider = (props) => {
     addTodo, // AddNewTodoForm
     removeTodo, // TodoItem, InnerList
     toggleComplete, // TodoItem, InnerList
-    selectedView, // Nav, SelectView
-    updateSelectedView, // SelectView
     selectedSort, // SelectSort, Daily, Weekly
     updateSelectedSort, // SelectSort
+    sortedTodoList, // Daily, Weekly
+    unsortedTodosToday, // TodoList
+    sortedTodosToday, // Daily, TodoList
+    selectedView, // Nav, SelectView
+    updateSelectedView, // SelectView
     isAddNewTodoClicked, // HomeMain, AddNewTodoForm
     setIsAddNewTodoClicked, // AddNewTodoForm
     showAddNewTodoForm, // HomeMain, HeaderShared, HeaderMonthly
-    hideAddNewTodoForm, // HomeMain, Daily, Weekly, Monthly, useUnmount.js
-    sortedTodoList // Daily, Weekly
+    hideAddNewTodoForm // HomeMain, Daily, Weekly, Monthly, useUnmount.js
   };
 
   return <GlobalContext.Provider value={value}>{props.children}</GlobalContext.Provider>;
---
+++ src/utils/sampleTodoList.js
@@ -0,0 +1,33 @@
+import shortid from 'shortid';
+
+// sample todoList
+// prettier-ignore
+const sampleTasks = ["Daily web development", "Get groceries for dinner", "Home exercise at 6pm", "Call Daniel for meeting", "House cleaning", "Tennis practice", "Do lundary", "Email Noah for update", "Finish monthly growth report", "Send in cacenllation letter", "Figure out vacation destination", "Fill in scholarship application" ]
+const todoTypes = ['important', 'work', 'study', 'other'];
+const d = new Date();
+
+export default sampleTasks.map((task, index) => {
+  let randomDate;
+  let randomType;
+  if (index < 2) {
+    randomDate = d.getDate();
+    randomType = todoTypes[0];
+  } else {
+    randomDate = d.getDate() + Math.floor(Math.random() * 7);
+    randomType = todoTypes[Math.floor(Math.random() * 4)];
+  }
+
+  return {
+    id: shortid.generate(),
+    task: task,
+    timestamp: d.getTime(),
+    type: randomType,
+    completed: Math.random() >= 0.5,
+    dateInfo: {
+      year: d.getFullYear(),
+      month: d.getMonth(),
+      date: randomDate,
+      day: new Date(d.getFullYear(), d.getMonth(), randomDate).toLocaleString('default', { weekday: 'long' })
+    }
+  };
+});

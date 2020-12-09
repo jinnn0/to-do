753136e2d6397175f463ab4699a0b452ce15dfcc
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-12-09 19:53:30 +0000

    Updates

---
+++ precache-manifest.b9b1aaf6af95611d9e93a6f65664d7fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ecf22d586d66fe758bdb6e7dc1fb074",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "e9b55ba7d08206b48e4f",
+    "url": "/to-do/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "5e27a59f3f710daa92f4",
+    "url": "/to-do/static/css/main.72173e52.chunk.css"
+  },
+  {
+    "revision": "e9b55ba7d08206b48e4f",
+    "url": "/to-do/static/js/2.3ab5f7b0.chunk.js"
+  },
+  {
+    "revision": "2931c61621a79f6806bee4bb9a9e61ac",
+    "url": "/to-do/static/js/2.3ab5f7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e27a59f3f710daa92f4",
+    "url": "/to-do/static/js/main.8f485234.chunk.js"
+  },
+  {
+    "revision": "b5d210bec7b313800160",
+    "url": "/to-do/static/js/runtime-main.2b5910fc.js"
+  },
+  {
+    "revision": "ed4b8ae607dd5e07930dd5e882442944",
+    "url": "/to-do/static/media/adjust.ed4b8ae6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to-do/precache-manifest.6f8292a8460aaa91f08a2fb433bab314.js"
+  "/to-do/precache-manifest.b9b1aaf6af95611d9e93a6f65664d7fd.js"
 );
 
 self.addEventListener('message', (event) => {
